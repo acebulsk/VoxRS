0264af4697222a12fd63abb8fa936d4d6e3db9a5
Alex Cebulski <alexcebulski@gmail.com>
2024-02-09 14:37:33 -0700

    mods to functions to work on linux and fix bug in hemi_view fn

--- tools/lrs_footprint_products.py
+++ tools/lrs_footprint_products.py
@@ -18,12 +18,12 @@ def hemi_view(batch_dir, ii, cn_coef=0.38):
     matplotlib.use('TkAgg')
     import matplotlib.pyplot as plt
 
-    rshmeta = pd.read_csv(batch_dir + "outputs\\rshmetalog.csv")
+    rshmeta = pd.read_csv(batch_dir + "rshmetalog.csv")
     imsize = rshmeta.img_size_px[0]  # assuming all images have same dimensions
     im_count = len(rshmeta)
 
     # load image pixel angle lookup
-    angle_lookup = pd.read_csv(batch_dir + "outputs\\phi_theta_lookup.csv")
+    angle_lookup = pd.read_csv(batch_dir + "phi_theta_lookup.csv")
     # build phi image (in radians)
     phi = np.full((imsize, imsize), np.nan)
     phi[(np.array(angle_lookup.y_index), np.array(angle_lookup.x_index))] = angle_lookup.phi
@@ -34,28 +34,29 @@ def hemi_view(batch_dir, ii, cn_coef=0.38):
 
     # import and calculate contact numbers, transmittance, and associated errors
     # see eq. A.16 in Staines & Pomeroy 2023 Supplementary
-    returns = tif.imread(batch_dir + "outputs\\" + rshmeta.file_name[ii])
+    im = tif.imread(batch_dir + rshmeta.file_name[ii])
     contacts = im * cn_coef
     transmit = np.exp(-contacts)
 
     plt.imshow(transmit[:, :, 0], interpolation='nearest')  # transmittance
     plt.imshow(transmit[:, :, 0], interpolation='nearest')  # transmittance error
+    plt.savefig(batch_dir + 'hemi_view_output.png')  # Save the image to a file
 
 
 def lrs_footprint_products(batch_dir, cn_coef, file_out=None, threshold=False):
 
     if file_out is True:
         if threshold:
-            file_out = batch_dir + "outputs\\rshmetalog_footprint_products_threshold" + str(threshold) + ".csv"
+            file_out = batch_dir + "rshmetalog_footprint_products_threshold" + str(threshold) + ".csv"
         else:
-            file_out = batch_dir + "outputs\\rshmetalog_footprint_products_alldeg.csv"
+            file_out = batch_dir + "rshmetalog_footprint_products_alldeg.csv"
 
-    rshmeta = pd.read_csv(batch_dir + "outputs\\rshmetalog.csv")
+    rshmeta = pd.read_csv(batch_dir + "rshmetalog.csv")
     imsize = rshmeta.img_size_px[0]  # assuming all images have same dimensions
     im_count = len(rshmeta)
 
     # load image pixel angle lookup
-    angle_lookup = pd.read_csv(batch_dir + "outputs\\phi_theta_lookup.csv")
+    angle_lookup = pd.read_csv(batch_dir + "phi_theta_lookup.csv")
     # build phi image (in radians)
     phi = np.full((imsize, imsize), np.nan)
     phi[(np.array(angle_lookup.y_index), np.array(angle_lookup.x_index))] = angle_lookup.phi
@@ -108,8 +109,8 @@ def lrs_footprint_products(batch_dir, cn_coef, file_out=None, threshold=False):
         cn_stack = np.full([imsize, imsize, z_num], np.nan)
         cn_std_stack = np.full([imsize, imsize, z_num], np.nan)
         for ii in tqdm(range(0, z_num), desc="img chunk" + str(zz + 1), ncols=100, leave=True):
-            # cn_stack[:, :, ii] = tif.imread(batch_dir + "outputs\\" + rshmeta.file_name[ii])[:, :, 0] * cn_coef
-            im = tif.imread(batch_dir + "outputs\\" + rshmeta.file_name[zz * z_step + ii])
+            # cn_stack[:, :, ii] = tif.imread(batch_dir + "" + rshmeta.file_name[ii])[:, :, 0] * cn_coef
+            im = tif.imread(batch_dir + "" + rshmeta.file_name[zz * z_step + ii])
             cn_stack[:, :, ii] = im[:, :, 0] * cn_coef
             # cn_std_stack[:, :, ii] = im[:, :, 1] * cn_coef  # for error analysis
 
@@ -262,160 +263,161 @@ def lrs_footprint_products(batch_dir, cn_coef, file_out=None, threshold=False):
     return footprint_df
 
 
-#
-# # # 19_149 -- snow_off
-snow_off_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_uf_r.25_px181_snow_off_dem_offset.25\\'
-snow_off_coef = 0.38686933  # python tx drop 5
-# snow_off_coef = 0.1921595  # optimized for cn dropping 5th
-# # cn_coef = 0.220319  # optimized for cn
-lrs_footprint_products(snow_off_dir, snow_off_coef, file_out=True)
-# threshold = 0.38438069
-# lrs_footprint_products(snow_off_dir, snow_off_coef, threshold=threshold, file_out=True)
+# #
+# # # # 19_149 -- snow_off
+# snow_off_dir = ''
+# snow_off_coef = 0.38686933  # python tx drop 5
+# # snow_off_coef = 0.1921595  # optimized for cn dropping 5th
+# # # cn_coef = 0.220319  # optimized for cn
+# lrs_footprint_products(snow_off_dir, snow_off_coef, file_out=True)
+# # threshold = 0.38438069
+# # lrs_footprint_products(snow_off_dir, snow_off_coef, threshold=threshold, file_out=True)
 
 
 #
 #
 # 045_050_052 -- snow_on
-snow_on_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_uf_r.25_px181_snow_on_dem_offset.25\\'
+snow_on_dir = '/media/alex/phd-data/cob-thesis/alex-example-processing/test1/outputs/hemisphere_resampling/'
 snow_on_coef = 0.37181197  # python tx drop 5
 # snow_on_coef = 0.1364611  # optimized for cn dropping 5th
 # cn_coef = 0.141832  # optimized for cn
-lrs_footprint_products(snow_on_dir, snow_on_coef, file_out=True)
+# lrs_footprint_products(snow_on_dir, snow_on_coef, file_out=True)
 
+hemi_view(snow_on_dir, 1, snow_on_coef)
 
 #
 #
 # # validation set at optimized coef
-# batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px1000_snow_off\\"
+# batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px1000_snow_off/"
 # cn_coef = 1
 # lrs_footprint_products(batch_dir, cn_coef, file_out=True)
-# batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px181_snow_off_max150m\\"
+# batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px181_snow_off_max150m/"
 # cn_coef = 1
 # lrs_footprint_products(batch_dir, cn_coef, file_out=True)
-# batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px181_snow_off\\"
+# batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px181_snow_off/"
 # cn_coef = 1
-# file_out = batch_dir + "outputs\\rshmetalog_footprint_products.csv"
+# file_out = batch_dir + "outputs/rshmetalog_footprint_products.csv"
 # lrs_footprint_products(batch_dir, cn_coef, file_out=file_out)
 # cn_coef = 0.38686933  # python tx drop 5
-# file_out = batch_dir + "outputs\\rshmetalog_footprint_products_opt.csv"
+# file_out = batch_dir + "outputs/rshmetalog_footprint_products_opt.csv"
 # lrs_footprint_products(batch_dir, cn_coef, file_out=file_out)
 # opt_thresh = 0.4713883769260814  # tx mean of opt cn_coef
-# file_out = batch_dir + "outputs\\rshmetalog_footprint_products_opt_thresh.csv"
+# file_out = batch_dir + "outputs/rshmetalog_footprint_products_opt_thresh.csv"
 # lrs_footprint_products(batch_dir, cn_coef, file_out=file_out, threshold=opt_thresh)
 #
-# batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px1000_snow_on\\"
+# batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px1000_snow_on/"
 # cn_coef = 1
 # lrs_footprint_products(batch_dir, cn_coef, file_out=True)
-# batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px181_snow_on_max150m\\"
+# batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px181_snow_on_max150m/"
 # cn_coef = 1
 # lrs_footprint_products(batch_dir, cn_coef, file_out=True)
-# batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px181_snow_on\\"
+# batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px181_snow_on/"
 # cn_coef = 1
 # lrs_footprint_products(batch_dir, cn_coef, file_out=True)
 # cn_coef = 0.37181197  # python tx drop 5
-# file_out = batch_dir + "outputs\\rshmetalog_footprint_products_opt.csv"
+# file_out = batch_dir + "outputs/rshmetalog_footprint_products_opt.csv"
 # lrs_footprint_products(batch_dir, cn_coef, file_out=file_out)
 # opt_thresh = 0.6915388816825417
-# file_out = batch_dir + "outputs\\rshmetalog_footprint_products_opt_thresh.csv"
+# file_out = batch_dir + "outputs/rshmetalog_footprint_products_opt_thresh.csv"
 # lrs_footprint_products(batch_dir, cn_coef, file_out=file_out, threshold=opt_thresh)
 
 # optimization
-from scipy.optimize import minimize, dual_annealing
+# from scipy.optimize import minimize, dual_annealing
 
 
-photos_lai_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\hemispheres\\19_149\\clean\\sized\\thresholded\\LAI_parsed.dat"
-# photos_lai_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\hemispheres\\045_052_050\\LAI_045_050_052_parsed.dat"
-photos_lai = pd.read_csv(photos_lai_in)
-photos_lai.loc[:, "original_file"] = photos_lai.picture.str.replace("_r.jpg", ".JPG", case=False).str.replace(".jpg", ".JPG", case=False)
-photos_meta_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\hemispheres\\hemi_lookup_cleaned.csv"
-photos_meta = pd.read_csv(photos_meta_in)
-#
-# photos_meta$id <- as.numeric(rownames(photos_meta)) - 1
+# photos_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/thresholded/LAI_parsed.dat"
+# # photos_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/045_052_050/LAI_045_050_052_parsed.dat"
+# photos_lai = pd.read_csv(photos_lai_in)
+# photos_lai.loc[:, "original_file"] = photos_lai.picture.str.replace("_r.jpg", ".JPG", case=False).str.replace(".jpg", ".JPG", case=False)
+# photos_meta_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/hemi_lookup_cleaned.csv"
+# photos_meta = pd.read_csv(photos_meta_in)
+# #
+# # photos_meta$id <- as.numeric(rownames(photos_meta)) - 1
 
-photos = photos_lai.merge(photos_meta, left_on='original_file', right_on='filename')
-photos_strip = photos.loc[:, ["original_file", "transmission_s_1", "transmission_s_2", "transmission_s_3", "transmission_s_4", "transmission_s_5"]]
-photos = pd.melt(photos_strip, id_vars="original_file", value_name="transmission_s")
-photos.loc[:, "ringnum"] = photos.variable.str.replace("transmission_s_", "").astype(int)
-photos.loc[:, "solid_angle"] = 2 * np.pi * (np.cos((photos.ringnum - 1) * 15 * np.pi / 180) - np.cos(photos.ringnum * 15 * np.pi / 180))
-
-
-def tx_opt_coef(xx, mean_tx=False):
-    threshold = xx[0]
-    # cn_coef = xx[0]
-    cn_coef = 0.38686933  # snow off
-
-    batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px181_snow_off\\"
-    # batch_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\batches\\lrs_hemi_optimization_r.25_px181_snow_on\\"
-    # cn_coef = .42
-    # threshold = 0.5
-    # fpp = lrs_footprint_products(batch_dir, cn_coef, file_out=False, threshold=threshold)
-
-    def run_footprint_analysis(threshold=threshold):
-        fpp = lrs_footprint_products(batch_dir, cn_coef, file_out=False, threshold=threshold)
-        fpp_tx = fpp.loc[:, ["id", "lrs_tx_1", "lrs_tx_2", "lrs_tx_3", "lrs_tx_4", "lrs_tx_5"]]
-        # fpp_cn = fpp.loc[:, ["id", "lrs_cn_1", "lrs_cn_2", "lrs_cn_3", "lrs_cn_4", "lrs_cn_5"]]
-
-        lrs_tx = pd.melt(fpp_tx, id_vars="id", value_name="lrs_tx")
-        lrs_tx.loc[:, "ringnum"] = lrs_tx.variable.str.replace("lrs_tx_", "").astype(int)
-        tx = lrs_tx.merge(photos, left_on=["id", "ringnum"], right_on=["original_file", "ringnum"])
-        tx.loc[:, "tx_error"] = tx.lrs_tx - tx.transmission_s
-
-        tx_drop = tx.loc[tx.ringnum != 5, :]
-
-        # lrs_cn = pd.melt(fpp_cn, id_vars="id", value_name="lrs_cn")
-        # lrs_cn.loc[:, "ringnum"] = lrs_cn.variable.str.replace("lrs_cn_", "").astype(int)
-        # cn = lrs_cn.merge(photos, left_on=["id", "ringnum"], right_on=["original_file", "ringnum"])
-        # cn.loc[:, "cn_error"] = cn.lrs_cn - -np.log(cn.transmission_s)
-
-        return tx_drop
-
-    # calculate error stats
-
-    def wrmse(difdata, weights):
-        weights = weights / np.nansum(weights)
-        return np.sqrt(np.nansum(weights * (difdata ** 2)))
-
-    def wmb(difdata, weights):
-        weights = weights / np.nansum(weights)
-        return np.nansum(weights * difdata)
-
-    tx_drop = run_footprint_analysis()
-
-    t_mean = wmb(tx_drop.lrs_tx, tx_drop.solid_angle)
-
-    # tx_drop_thresh = run_footprint_analysis(threshold=t_mean)
-
-    # wmb(tx.tx_error, tx.solid_angle)
-    # wmb(cn.cn_error, cn.solid_angle)
-    # wrmse(cn.cn_error, cn.solid_angle)
-
-    if mean_tx:
-        return [wrmse(tx_drop.tx_error, tx_drop.solid_angle), t_mean]
-    else:
-        return wrmse(tx_drop.tx_error, tx_drop.solid_angle)
-
-res = minimize(tx_opt_coef, x0=np.array([0.5]))
-
-#
-# def callbackF(xi):
-#     print('{0: 3.6f}'.format(xi[0]))
-#
-# print('{0:9s}'.format('X0'))
-#
-#
-# w0 = np.array([0.5])
-# [popt, fopt, gopt, Bopt, func_calls, grad_calls, warnflg] = \
-#     fmin_bfgs(tx_opt_coef, w0, callback=callbackF, maxiter=50, full_output=True, retall=False)
+# photos = photos_lai.merge(photos_meta, left_on='original_file', right_on='filename')
+# photos_strip = photos.loc[:, ["original_file", "transmission_s_1", "transmission_s_2", "transmission_s_3", "transmission_s_4", "transmission_s_5"]]
+# photos = pd.melt(photos_strip, id_vars="original_file", value_name="transmission_s")
+# photos.loc[:, "ringnum"] = photos.variable.str.replace("transmission_s_", "").astype(int)
+# photos.loc[:, "solid_angle"] = 2 * np.pi * (np.cos((photos.ringnum - 1) * 15 * np.pi / 180) - np.cos(photos.ringnum * 15 * np.pi / 180))
 
 
-# tx_wrmse, tx_mean = tx_opt_coef(popt, mean_tx=True)
+# def tx_opt_coef(xx, mean_tx=False):
+#     threshold = xx[0]
+#     # cn_coef = xx[0]
+#     cn_coef = 0.38686933  # snow off
 
-res = dual_annealing(tx_opt_coef, bounds=[(0.1, 0.6)], x0=np.array([0.38]), maxiter=50)
+#     batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px181_snow_off/"
+#     # batch_dir = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/ray_sampling/batches/lrs_hemi_optimization_r.25_px181_snow_on/"
+#     # cn_coef = .42
+#     # threshold = 0.5
+#     # fpp = lrs_footprint_products(batch_dir, cn_coef, file_out=False, threshold=threshold)
+
+#     def run_footprint_analysis(threshold=threshold):
+#         fpp = lrs_footprint_products(batch_dir, cn_coef, file_out=False, threshold=threshold)
+#         fpp_tx = fpp.loc[:, ["id", "lrs_tx_1", "lrs_tx_2", "lrs_tx_3", "lrs_tx_4", "lrs_tx_5"]]
+#         # fpp_cn = fpp.loc[:, ["id", "lrs_cn_1", "lrs_cn_2", "lrs_cn_3", "lrs_cn_4", "lrs_cn_5"]]
+
+#         lrs_tx = pd.melt(fpp_tx, id_vars="id", value_name="lrs_tx")
+#         lrs_tx.loc[:, "ringnum"] = lrs_tx.variable.str.replace("lrs_tx_", "").astype(int)
+#         tx = lrs_tx.merge(photos, left_on=["id", "ringnum"], right_on=["original_file", "ringnum"])
+#         tx.loc[:, "tx_error"] = tx.lrs_tx - tx.transmission_s
+
+#         tx_drop = tx.loc[tx.ringnum != 5, :]
+
+#         # lrs_cn = pd.melt(fpp_cn, id_vars="id", value_name="lrs_cn")
+#         # lrs_cn.loc[:, "ringnum"] = lrs_cn.variable.str.replace("lrs_cn_", "").astype(int)
+#         # cn = lrs_cn.merge(photos, left_on=["id", "ringnum"], right_on=["original_file", "ringnum"])
+#         # cn.loc[:, "cn_error"] = cn.lrs_cn - -np.log(cn.transmission_s)
+
+#         return tx_drop
+
+#     # calculate error stats
+
+#     def wrmse(difdata, weights):
+#         weights = weights / np.nansum(weights)
+#         return np.sqrt(np.nansum(weights * (difdata ** 2)))
+
+#     def wmb(difdata, weights):
+#         weights = weights / np.nansum(weights)
+#         return np.nansum(weights * difdata)
+
+#     tx_drop = run_footprint_analysis()
+
+#     t_mean = wmb(tx_drop.lrs_tx, tx_drop.solid_angle)
+
+#     # tx_drop_thresh = run_footprint_analysis(threshold=t_mean)
+
+#     # wmb(tx.tx_error, tx.solid_angle)
+#     # wmb(cn.cn_error, cn.solid_angle)
+#     # wrmse(cn.cn_error, cn.solid_angle)
+
+#     if mean_tx:
+#         return [wrmse(tx_drop.tx_error, tx_drop.solid_angle), t_mean]
+#     else:
+#         return wrmse(tx_drop.tx_error, tx_drop.solid_angle)
+
+# res = minimize(tx_opt_coef, x0=np.array([0.5]))
+
+# #
+# # def callbackF(xi):
+# #     print('{0: 3.6f}'.format(xi[0]))
+# #
+# # print('{0:9s}'.format('X0'))
+# #
+# #
+# # w0 = np.array([0.5])
+# # [popt, fopt, gopt, Bopt, func_calls, grad_calls, warnflg] = \
+# #     fmin_bfgs(tx_opt_coef, w0, callback=callbackF, maxiter=50, full_output=True, retall=False)
+
+
+# # tx_wrmse, tx_mean = tx_opt_coef(popt, mean_tx=True)
+
+# res = dual_annealing(tx_opt_coef, bounds=[(0.1, 0.6)], x0=np.array([0.38]), maxiter=50)
 
 #
 # # create raster products
 # df = pd.read_csv(file_out)
-# point_ids_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\hemi_grid_points\\mb_65_r.25m\\dem_r.25_point_ids.tif"
+# point_ids_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/hemi_grid_points/mb_65_r.25m/dem_r.25_point_ids.tif"
 # colnames = [
 #     "lrs_cn_1",
 #     "lrs_cn_2",
@@ -431,9 +433,9 @@ res = dual_annealing(tx_opt_coef, bounds=[(0.1, 0.6)], x0=np.array([0.38]), maxi
 #
 #
 # # create raster products
-# file_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\batches\\mb_15_1m_pr.15_os10\\outputs\\LAI_parsed.dat"
+# file_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/batches/mb_15_1m_pr.15_os10/outputs/LAI_parsed.dat"
 # df = pd.read_csv(file_in)
-# point_ids_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\hemi_grid_points\\mb_65_1m\\1m_dem_point_ids.tif"
+# point_ids_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/hemi_grid_points/mb_65_1m/1m_dem_point_ids.tif"
 # colnames = [
 #     "contactnum_1",
 #     "contactnum_2",
